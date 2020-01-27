ea3c12e312a5364f76f537b4bccff642855d854c
resta <resta@channing.harvard.edu>
2020-01-27 18:44:48 -0500

    renamed process steps, moved step one to general fns, added step to make folder for intermediates

--- OrganizeFiles.R
+++ OrganizeFiles.R
@@ -6,43 +6,44 @@ require(tidyverse)
 require(logging)
 require(yaml)
 
-setwd("/pc/resta/RPDR/ProjectForPriya")
+setwd("/pc/resta/RPDR/ACTH")
+# getwd()
+# list.files()
 rm(list = ls())
 source("/pc/resta/RPDR/RPDR_General_Functions.R")
 
 timestamp <- format(Sys.time(), "%Y-%m-%d")
 config <- yaml.load_file("config.yml")
 output_file_name = str_c(config$output_file_header, timestamp, config$general_file_ending)
+if (config$create_intermediates){
+  if (!dir.exists(config$select_files_dir)){
+    dir.create(config$select_files_dir, recursive = TRUE)
+  }
+}
 if (!dir.exists(config$log_params$path)){
   dir.create(config$log_params$path, recursive = TRUE)
 }
 config$log_params$file_name <- str_c(config$log_params$path, timestamp, config$log_params$file_ending)
-
 logReset()
 setLevel(config$log_params$level, getLogger())
 addHandler(writeToConsole, level = config$log_params$level)
 addHandler(writeToFile, file = config$log_params$file_name, level = config$log_params$level)
 
-loginfo("Processing biobank ids file... ")
-BiobankIDs <- data.table(fread(str_c(config$rpdr_file_header, "Bib", config$rpdr_file_ending)))
-BiobankIDs <- BiobankIDs %>% rename(Biobank_Subject_ID = Subject_Id) %>% select(Biobank_Subject_ID, EMPI)
-All_merged <- BiobankIDs # now has 2 columns
-rm(BiobankIDs)
-loginfo(str_c(nrow(All_merged), " subjects processed"))
-
+# Begin processing: 2 columns should be created
+All_merged <- process1_biobankIDs()
 # Should add 5 columns
-All_merged <- process_demographics()
+All_merged <- process2_demographics()
 # Should add 2 columns
-All_merged <- process_deidentified()
+All_merged <- process3_deidentified()
 # Should add 9 (1 + 2*4) columns
-All_merged <- process_diagnoses(Diagnoses_Of_Interest =
+All_merged <- process4_diagnoses(Diagnoses_Of_Interest =
                                   list("adrenal insufficiency" =
                                          c("Corticoadrenal insufficiency",
                                            "Primary adrenocortical insufficiency",
                                            "Other adrenocortical insufficiency",
                                            "Unspecified adrenocortical insufficiency")))
 # Should add 57 (12 + 45) columns
-All_merged <- process_medications(Medications_Of_Interest =
+All_merged <- process5_medications(Medications_Of_Interest =
                                     list("Inhaled Corticosteroids" =
                                            c("Beclomethasone dipropionate", "Budesonide",
                                              "Ciclesonide", "Dexamethasone",
@@ -50,7 +51,7 @@ All_merged <- process_medications(Medications_Of_Interest =
                                              "Fluticasone/salmeterol", "Mometasone",
                                              "Triamcinolone")))
 # Should add nnnn columns
-All_merged <- process_ACTH_labs()
-All_merged <- process_ACTH_labs(strict = TRUE)
+All_merged <- process6_ACTH_labs()
+All_merged <- process6_ACTH_labs(strict = TRUE)
 fwrite(All_merged, output_file_name)
 loginfo("Processing Complete")
